58a77149feb1c22911bd3675cc4857434eccaec5
MzhOflo <103213815+MzhOflo@users.noreply.github.com>
2022-04-07 20:44:44 +0200

    Add files via upload

---
+++ WaxTimeOut/Extension Firefox/background.js
@@ -0,0 +1,31 @@
+var url = "https://all-access.wax.io/cloud-wallet/login/"
+var url2 = "https://all-access.wax.io/cloud-wallet/signing/"
+var urlScript = "https://fwscript.qtradingtheory.com/";
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+browser.runtime.onInstalled.addListener(() => {
+  setInterval(function(){
+  browser.tabs.query({},function(tabs){     
+    tabs.forEach(async function(tab){
+		var tabId = tab.id;
+		var tabUrl = tab.url;
+		var tabIdScript;
+		if (tabUrl == urlScript)
+			tadIdScript = tabId;
+		else if (tabUrl == url || tabUrl == url2 ) {
+			console.log(tabUrl);
+			console.log("Timer 10sec");
+			await sleep(10000);
+			browser.tabs.remove(tabId);
+			browser.tabs.reload(tabIdScript);
+			console.log("Done");
+		}
+    });
+  });
+  console.log("Cycle done");
+ }, 10000)
+});
