a29ef72020fe7f40880e0feb2d2898003633b8f4
MzhOflo <103213815+MzhOflo@users.noreply.github.com>
2022-04-07 21:48:15 +0200

    Add files via upload

--- WaxTimeOut/Extension Chrome/background.js
+++ WaxTimeOut/Extension Chrome/background.js
@@ -6,26 +6,25 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function main(tab){
+	var tabId = tab.id;
+	var tabUrl = tab.url;
+	var tabIdScript;
+	console.log(tabUrl);
+	if (tabUrl == urlScript)
+		tadIdScript = tabId;
+	else if (tabUrl == url || tabUrl == url2 ) {
+		console.log(tabUrl);
+		console.log("Timer 10sec");
+		await sleep(10000);
+		chrome.tabs.remove(tabId);
+		chrome.tabs.reload(tabIdScript);
+		console.log("Done");
+	}
+}
 
-chrome.runtime.onInstalled.addListener(() => {
-  setInterval(function(){
-  chrome.tabs.query({},function(tabs){     
-    tabs.forEach(async function(tab){
-		var tabId = tab.id;
-		var tabUrl = tab.url;
-		var tabIdScript;
-		if (tabUrl == urlScript)
-			tadIdScript = tabId;
-		else if (tabUrl == url || tabUrl == url2 ) {
-			console.log(tabUrl);
-			console.log("Timer 10sec");
-			await sleep(10000);
-			chrome.tabs.remove(tabId);
-			chrome.tabs.reload(tabIdScript);
-			console.log("Done");
-		}
-    });
-  });
+chrome.runtime.onStartup.addListener(() => {
+  setInterval(function(){chrome.tabs.query({},function(tabs){tabs.forEach(main(tab))});
   console.log("Cycle done");
  }, 10000)
 });
